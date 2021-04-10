50fc68f2df5c52d82fe3e55b4e500b24ef5e366b
Sayantan Mandal <sanu_xze@yahoo.com>
2021-04-10 20:01:33 +0530

    del staticfiles folder

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -134,13 +134,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
